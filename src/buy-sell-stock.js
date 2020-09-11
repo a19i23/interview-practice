3ad635aa46da4e86653c1ec83c39669657b57e4a
Alan Villa <a19i23@hotmail.com>
2020-09-11 15:44:15 -0700

    adding a brute force approach and more test cases

--- src/buy-sell-stock.js
+++ src/buy-sell-stock.js
@@ -1,10 +1,36 @@
+// Brute force O(n)^2
+// function stockBuySell(prices){
+//   if(prices.length < 2) return "Need more than one stock price"
 
+//   let highestPrice = 0;
+//   let lowestPrice = Number.MAX_SAFE_INTEGER;
+//   let lowPriceIndex = 0;
+//   let highPriceIndex = 0;
+//   let maxProfit = Number.MIN_SAFE_INTEGER;
+
+//   for (let i = 0; i < prices.length; i++) {
+//     for (let j=i+1; j<prices.length; j++){  
+
+//       const currentProfit = prices[j] - prices[i]
+//       if(currentProfit > maxProfit){
+//         maxProfit = currentProfit;
+//         highPriceIndex = j;
+//         lowPriceIndex = i;
+//         highestPrice = prices[j];
+//         lowestPrice = prices[i];
+//       }
+//     }
+//   }
+//   return `Buy on day ${lowPriceIndex+1}, sell on ${highPriceIndex + 1}`
+// }
+
+// O(n) time complexity
 function stockBuySell(prices){
   let lowestPrice = Number.MAX_SAFE_INTEGER;
-  let lowestPricePosition = -1;
-  let highestPricePosition = -1;
+  let lowestPricePosition = 0;
+  let highestPricePosition = 0;
   let highestPrice = 0;
-  let maxProfit = 0; 
+  let maxProfit = Number.MIN_SAFE_INTEGER; 
 
   if(!(prices.length >1))return "Need more than one stock price"
 
@@ -16,7 +42,7 @@ function stockBuySell(prices){
     }
     
     const nextDayPrice = prices[i+1];
-    if (nextDayPrice && nextDayPrice > price){
+    if (nextDayPrice && nextDayPrice > price && nextDayPrice > highestPrice){
       highestPrice = nextDayPrice;
       highestPricePosition = i+1;
       const profit = nextDayPrice - price;
@@ -30,4 +56,18 @@ function stockBuySell(prices){
   return `buy on day ${lowestPricePosition +1}, sell on day ${highestPricePosition +1}`
 }
 
-console.log(stockBuySell([110, 180, 260, 40, 310, 535, 695]));
+console.log(stockBuySell([110]));
+
+// console.log(stockBuySell([20, 10])) //Buy on day 1, sell on 2 -- this one is questionable
+
+console.log(stockBuySell([20, 500, 50, 100])) //Buy on day 1, sell on 2  - max profit early on
+console.log(stockBuySell([20, 100, 50, 500])) //Buy on day 1, sell on 4 - max profit throughout range
+
+
+console.log(stockBuySell([20, 1000, 50, 500])) //Buy on day 1, sell on 2
+
+console.log(stockBuySell([20, 100, 10, 50, 500])) //Buy on day 3, sell on 5
+
+console.log(stockBuySell([20, 100, 0, 50, 500])) //Buy on day 3, sell on 5
+
+console.log(stockBuySell([110, 180, 260, 40, 310, 535, 695])); //Buy on day 4, sell on 7
