5761bc7373b67cfc3b1abcbbca2b383f4aefaae4
Alan Villa <a19i23@hotmail.com>
2020-09-23 22:27:43 -0700

    adding cleaner solution

--- src/reverseStringInPlace.js
+++ src/reverseStringInPlace.js
@@ -24,3 +24,22 @@ const inputArray = ['a','l','a','n','a'];
 
 console.log(reverseStringInPlace(inputArray));
 
+
+// cleaner solution
+// function reverse(arrayOfChars) {
+
+//   let leftIndex = 0;
+//   let rightIndex = arrayOfChars.length - 1;
+
+//   while (leftIndex < rightIndex) {
+
+//     // Swap characters
+//     const temp = arrayOfChars[leftIndex];
+//     arrayOfChars[leftIndex] = arrayOfChars[rightIndex];
+//     arrayOfChars[rightIndex] = temp;
+
+//     // Move towards middle
+//     leftIndex++;
+//     rightIndex--;
+//   }
+// }
