9d7ed7f1264c54574d03092e4f8078d52f7a9799
Alan Villa <a19i23@hotmail.com>
2021-08-03 20:41:36 -0700

    latest commit

--- src/appleStocks.js
+++ src/appleStocks.js
@@ -1,22 +1,22 @@
-function getMaxProfit(prices){
+function getMaxProfit(stockPrices){
   let lowPriceIndex =  0;
   let highPriceIndex = 0;
   let maxProfit= Number.NEGATIVE_INFINITY;
 
-  for (let index = 0; index < prices.length; index++) {
-    if (prices[index] < prices[lowPriceIndex]){
+  for (let index = 0; index < stockPrices.length; index++) {
+    if (stockPrices[index] < stockPrices[lowPriceIndex]){
       lowPriceIndex = index;
     }
-    if(prices[index +1]> prices[highPriceIndex]){
+    if(stockPrices[index +1]> stockPrices[highPriceIndex]){
       highPriceIndex = index + 1;
     }
-    const currentProfit = prices[highPriceIndex] - prices[lowPriceIndex];
+    const currentProfit = stockPrices[highPriceIndex] - stockPrices[lowPriceIndex];
 
     if (currentProfit > maxProfit){
       maxProfit = currentProfit
     }
 
-    if (index+1 === prices.length-1) {
+    if (index+1 === stockPrices.length-1) {
       return maxProfit;
     }
   }
@@ -27,3 +27,38 @@ const stockPrices = [10, 7, 5, 8, 11, 9];
 const maxProfit = getMaxProfit(stockPrices);
 
 console.log(maxProfit);
+
+//Solution
+// function getMaxProfit(stockPrices) {
+//   if (stockPrices.length < 2) {
+//     throw new Error('Getting a profit requires at least 2 prices');
+//   }
+
+//   // We'll greedily update minPrice and maxProfit, so we initialize
+//   // them to the first price and the first possible profit
+//   let minPrice = stockPrices[0];
+//   let maxProfit = stockPrices[1] - stockPrices[0];
+
+//   // Start at the second (index 1) time
+//   // We can't sell at the first time, since we must buy first,
+//   // and we can't buy and sell at the same time!
+//   // If we started at index 0, we'd try to buy *and* sell at time 0.
+//   // this would give a profit of 0, which is a problem if our
+//   // maxProfit is supposed to be *negative*--we'd return 0.
+//   for (let i = 1; i < stockPrices.length; i++) {
+//     const currentPrice = stockPrices[i];
+
+//     // See what our profit would be if we bought at the
+//     // min price and sold at the current price
+//     const potentialProfit = currentPrice - minPrice;
+
+//     // Update maxProfit if we can do better
+//     maxProfit = Math.max(maxProfit, potentialProfit);
+
+//     // Update minPrice so it's always
+//     // the lowest price we've seen so far
+//     minPrice = Math.min(minPrice, currentPrice);
+//   }
+
+//   return maxProfit;
+// }
---
+++ src/meshMessage.js
@@ -0,0 +1,87 @@
+class Queue {
+  constructor() {
+    this.queue = [];
+    this.size = 0;
+  }
+
+  enqueue(item) {
+    this.queue.unshift(item);
+    this.size += 1;
+  }
+
+  dequeue() {
+    this.size -= 1;
+    return this.queue.pop();
+  }
+}
+
+function reconstructPath(howWeReachedNodes, startNode, endNode) {
+
+  const reversedShortestPath = [];
+
+  // Start from the end of the path and work backwards
+  let currentNode = endNode;
+
+  while (currentNode !== null) {
+    reversedShortestPath.push(currentNode);
+    currentNode = howWeReachedNodes[currentNode];
+  }
+
+  // Reverse our path to get the right order
+  return reversedShortestPath.reverse(); // No longer reversed
+}
+
+function getPath(graph, startNode, endNode) {
+
+  if (!graph.hasOwnProperty(startNode)) {
+    throw new Error('Start node not in graph!');
+  }
+  if (!graph.hasOwnProperty(endNode)) {
+    throw new Error('End node not in graph!');
+  }
+
+  const nodesToVisit = new Queue();
+  nodesToVisit.enqueue(startNode);
+
+  // Keep track of how we got to each node
+  // We'll use this to reconstruct the shortest path at the end
+  // We'll ALSO use this to keep track of which nodes we've
+  // already visited
+  const howWeReachedNodes = {};
+  howWeReachedNodes[startNode] = null;
+
+  while (nodesToVisit.size > 0) {
+    const currentNode = nodesToVisit.dequeue();
+
+    // Stop when we reach the end node
+    if (currentNode === endNode) {
+      return reconstructPath(howWeReachedNodes, startNode, endNode);
+    }
+
+    graph[currentNode].forEach(neighbor => {
+      if (!howWeReachedNodes.hasOwnProperty(neighbor)) {
+        nodesToVisit.enqueue(neighbor);
+        howWeReachedNodes[neighbor] = currentNode;
+      }
+    });
+  }
+
+  // If we get here, then we never found the end node
+  // so there's NO path from startNode to endNode
+  return null;
+}
+
+const graph = {
+  'a': ['b', 'c', 'd'],
+  'b': ['a', 'd'],
+  'c': ['a', 'e'],
+  'd': ['a', 'b'],
+  'e': ['c'],
+  'f': ['g'],
+  'g': ['f']
+};
+
+let desc = 'two hop path 1';
+let actual = getPath(graph, 'a', 'e');
+let expected = ['a', 'c', 'e'];
+assertDeepEqual(actual, expected, desc);
